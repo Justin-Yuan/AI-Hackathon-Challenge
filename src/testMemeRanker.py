f8313cb7f5b2ad9c07460611c612dedeead54920
Justin-Yuan <justin.zcyuan@gmail.com>
2017-06-25 01:10:32 -0400

    initialized Ranker test file

---
+++ src/testMemeRanker.py
@@ -0,0 +1,34 @@
+from __future__ import print_function 
+from ImageAnalyzer import ImageAnalyzer
+from MemeRanker import MemeRanker
+
+if __name__ == '__main__':
+    # Test ImageAnalyzer 
+    
+    imgAnalyzer = ImageAnalyzer()
+    print('ok')
+
+    img_path = '../imgs/test2.jpg'
+
+    length, top_sorted_results, original_results = imgAnalyzer.decode_emotion(img_path)
+
+    print("emotion")
+
+    title, description, keywords = imgAnalyzer.decode_context(img_path)
+
+    print("context")
+
+    print(length)
+    print(top_sorted_results)
+    print(original_results)
+    print(title)
+    print(description)
+    print(keywords)
+
+    print("Analyzer test success")
+
+    memeRanker = MemeRanker("../Quotes Database/quotes_analysis_results.p")
+
+    print('Ranker loaded success')
+
+    
